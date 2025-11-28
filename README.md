a4af9741de9a40e8008ad28bfc48a4593fb287a1
Abhay pratap singh <singhpratap1055@gmail.com>
2025-11-28 22:45:27 +0530

    ..

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# 100-DAYS-OF-CODE
-100 Days of Code" is a popular coding challenge where you commit to coding at least one hour every day for 100 days straight, and share your progress—typically on platforms like Twitter (or X), GitHub, or a blog—with the hashtag #100DaysOfCode.
+# programming.c
---
+++ main.c
@@ -0,0 +1,24 @@
+//Q1: Write a program to input two numbers and display their sum.
+
+
+#include<stdio.h>
+int main(){
+    int a,b,c;
+    printf("enter a number");
+    scanf("%d", &a);
+    printf("enter a number");
+    scanf("%d", &b);
+    c=a+b;
+    printf("the sum of c is %d",c);
+    return 0;
+}
+
+    
+        
+        
+     
+    
+    
+
+    
+    
